e6c36102760e2ff62a110416b2c8312a05b13b36
dyune <djhwang0777@gmail.com>
2024-01-27 19:32:41 -0500

    v2 django backend, user auth frame added

--- tmapp/trademap/urls.py
+++ tmapp/trademap/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.authenticate, name = "login"),
-    path("home", views.load_home, name = "home"),
-    path("home/make-a-post", views.make_post, name = "create"),
-    path("home/<str:slug>", views.post_details, name = "load"),
+    path("", views.index, name = "home"),
+    path('login/', views.user_login, name = "login"),
+    path('signup/', views.user_signup, name = "signup"),
+    path('logout/', views.user_signup, name = "logout"),
+    path("make-a-post/", views.make_post, name = "create"),
+    path("<str:slug>/", views.post_details, name = "load"),
 ]
